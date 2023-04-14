d62819b15421fd4c663cbc58ba94f4dc9afd719a
Syun'ichi Shiraiwa <shiraiwa@princeton.edu>
2023-04-14 00:01:56 -0400

    strumpack_solve...looks okay

--- strumpack_solve/strumpack_solve.cpp
+++ strumpack_solve/strumpack_solve.cpp
@@ -1,26 +1,47 @@
-StrumpackSolver::~StrumpackSolver()
+#include "StrumpackSparseSolver.h"
+#include "strumpack_solve.hpp"
+
+namespace libstrumpack_solve{
+  
+template <STRUMPACK_PRECISION T1, typename T2>
+StrumpackSolver<T1, T2>::~StrumpackSolver()
 {
   if (success) {
-     STRUMPACK_destroy(&spss);
+     STRUMPACK_destroy(spss);
   }
 }
 
-void StrumpackSolver::set_csr_matrix0(int N, int *row_ptr, int *col_ind, T2 *values, int symmetric_pattern){
-   STRUMPACK_set_csr_matrix(spss,  &N, (void*) row_ptr,(void*) col_ind,
+
+template <STRUMPACK_PRECISION T1, typename T2>
+void StrumpackSolver<T1, T2>::set_csr_matrix0(int N,
+					      int *row_ptr,
+					      int *col_ind,
+					      T2 *values,
+					      int symmetric_pattern){
+   STRUMPACK_set_csr_matrix(*spss,  &N, (void*) row_ptr,(void*) col_ind,
 		(void*) values, symmetric_pattern);
 }
 
-void StrumpackSolver::set_distributed_csr_matrix0(int local_rows, const int* row_ptr,
-                                     const int* col_ind, const T2 *values,
-				     const int* dist,   int symmetric_pattern){
 
-  STRUMPACK_set_distributed_csr_matrix(spss,
+template <STRUMPACK_PRECISION T1, typename T2>
+void StrumpackSolver<T1, T2>::set_distributed_csr_matrix0(int local_rows,
+							  const int* row_ptr,
+							  const int* col_ind,
+							  const T2 *values,
+							  const int* dist,
+							  int symmetric_pattern){
+
+  STRUMPACK_set_distributed_csr_matrix(*spss,
 		       &local_rows, (const void*) row_ptr,
 		       (const void*) col_ind, (const void*) values,
 		       (const void*) dist, symmetric_pattern);
 }
-   
-STRUMPACK_RETURN_CODE StrunpackSolver::solve(T2 *b, T2 *x, int use_initial_guess){
-   return STRUMPACK_solve(spss, (const void*) b, (void*) x, use_initial_guess);
-}  
 
+
+
+template <STRUMPACK_PRECISION T1, typename T2>
+STRUMPACK_RETURN_CODE StrumpackSolver<T1, T2>::solve(T2 *b, T2 *x, int use_initial_guess){
+   return STRUMPACK_solve(*spss, (const void*) b, (void*) x, use_initial_guess);
+}
+  
+} /* end of namespace */
